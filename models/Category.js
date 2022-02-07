e9df7fbcc069063d0f3733a05e96d45147f1f546
Luis Zabala <zabalaluis1234@gmail.com>
2022-02-07 00:37:36 -0400

    modified toJSON method on category

--- models/Category.js
+++ models/Category.js
@@ -4,4 +4,10 @@ const CategorySchema = Schema({
   name: String,
 })
 
+CategorySchema.methods.toJSON = function () {
+  const { __v, _id, ...category } = this.toObject()
+  category.uid = _id
+  return category
+}
+
 module.exports = model('Categories', CategorySchema)
